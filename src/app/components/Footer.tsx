8b8cc110661345188e0fda08bf0e9e51a1fee368
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-02-25 23:44:02 +0530

    Fixing footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,13 @@
 import styles from './styles/Footer.module.scss';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <p>© KIITBites 2024. All rights reserved.</p>
-  </footer>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Get the current year
+
+  return (
+    <footer className={styles.footer}>
+      <p>© KIITBites {currentYear}. All rights reserved.</p>
+    </footer>
+  );
+};
 
 export default Footer;
