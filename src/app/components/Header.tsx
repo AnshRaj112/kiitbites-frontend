e35e339717086edf0e0046f2a0d988f8e4569be5
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-03-03 03:10:20 +0530

    fixing spelling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
 
         <div className={styles.navItem} onClick={() => router.push("/login")}>
           <IoPersonOutline size={20} />
-          <span>LogIn</span>
+          <span>Login</span>
         </div>
 
         <div className={styles.navItem} onClick={() => router.push("/cart")}>
