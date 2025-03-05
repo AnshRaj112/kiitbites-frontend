097163c6d7e957ba405acc7578fb9a6083811f88
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-03-05 11:06:59 +0530

    fixing deployment

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -127,7 +127,7 @@ const Header: React.FC = () => {
               initial={{ x: "100%", opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               exit={{ x: "100%", opacity: 0 }}
-              transition={{ duration: 0.4, ease: "easeInOut" }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
             >
               <div className={styles.menuBox}>
                 <div
