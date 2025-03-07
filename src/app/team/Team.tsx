642719c732c3686cce9c1e52cb4a891786c40deb
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-03-07 15:22:38 +0530

    Fixing deployment

---
+++ src/app/team/Team.tsx
@@ -0,0 +1,9 @@
+export default function TeamPage() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is the about page.</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/team/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import TeamPage from "./Team"; // Adjust the path if necessary
+
+export default function Team() {
+  return <TeamPage />;
+}
