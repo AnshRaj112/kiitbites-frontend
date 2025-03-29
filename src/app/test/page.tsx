042003522c6aee069369019b0a91ee86f55ed9dc
amritiano <156211338+amritiano@users.noreply.github.com>
2025-03-30 00:34:07 +0530

    Adding a fucking comment

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,4 @@
+// Do not fucking dare to change any fucking thing cuz you are making your own fucking market
 import Dishes from "../components/Dishes";
 import RestaurantComponent from "../components/RestaurantCard";
 
