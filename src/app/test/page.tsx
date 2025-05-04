a1007dab52cbbde33513502cc67beb31bfe75690
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-05-04 22:56:39 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,12 @@ import RestaurantComponent from "../components/RestaurantCard";
 export default function TestPage(){
   return (
     <div>
-      <Dishes />
+      <DishCard 
+        dishName="Paneer Butter Masala"
+        price={199}
+        image="https://res.cloudinary.com/dt45pu5mx/image/upload/v1744159414/Screenshot_2025-04-09_055439_aucvs9.png"
+        variant="list" // optional
+    />
       <RestaurantComponent />
     </div>
   )
