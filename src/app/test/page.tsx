aedb13c0ba684d004408422fab41300644afe17f
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-05-04 22:47:25 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,3 @@
-// Do not fucking dare to change any fucking thing cuz you are making your own fucking market
 import Dishes from "../components/DishCard";
 import RestaurantComponent from "../components/RestaurantCard";
 
