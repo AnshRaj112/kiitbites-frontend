0726913e4f78563da52db73872029a68f556878a
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-05-04 22:57:59 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,7 +10,17 @@ export default function TestPage(){
         image="https://res.cloudinary.com/dt45pu5mx/image/upload/v1744159414/Screenshot_2025-04-09_055439_aucvs9.png"
         variant="list" // optional
     />
-      <RestaurantComponent />
+      <RestaurantComponent 
+  restaurantName="Spice Garden"
+  restaurantImage="https://res.cloudinary.com/dt45pu5mx/image/upload/v1744159414/spice_garden.png"
+  cuisine="North Indian, Chinese"
+  location="KIIT Square, Bhubaneswar"
+  distance="1.2 km away"
+  rating={4.3}
+  description="Authentic flavors with a modern twist"
+  variant="highlight" // optional
+/>
+
     </div>
   )
 }
