4c8a1c6b3e4d55f92571de2c6943d16257ad9afe
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-05-30 22:32:02 +0530

    Fixing Metadata and title for everything

--- src/app/about/metadata.ts
+++
@@ -1,6 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About Us",
-  description: "Learn more about BitesBay",
-}; 
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,31 @@
-"use client";
+import AboutPage from "./About";
+import { Metadata } from "next";
 
-import AboutPage from "./About"; // Adjust the path if necessary
+export const metadata: Metadata = {
+  title: "About Us | BitesBay",
+  description: "Learn about BitesBay, your trusted partner for modern food ordering in college campuses. Discover our mission and what we offer.",
+  keywords: "About BitesBay, College Food Ordering, Campus Dining Tech, Inventory Management",
+  openGraph: {
+    title: "About BitesBay",
+    description: "Streamlining food orders and inventory for college food courts.",
+    images: ["https://bitebay.in/og-image.jpg"],
+    url: "https://bitesbay.come/about",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "About Us | BitesBay",
+    description: "Discover how BitesBay is redefining food ordering and inventory management for colleges.",
+    images: ["https://bitebay.in/twitter-card.jpg"],
+  },
+  alternates: {
+    canonical: "https://bitebay.in/about",
+  },
+};
 
 export default function About() {
   return (
-    <div> 
+    <div>
       <AboutPage />
     </div>
   );
